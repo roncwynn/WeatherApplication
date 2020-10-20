ceb20d47e87bfdaec7a22e9157d90cbaf4b02531
Ron Wynn <rcwynn@gmail.com>
2020-10-20 17:26:05 -0400

    WIP.  Converting BuildAddressString to use on Address class

--- WeatherApplication/Models/AddressModel.cs
+++ WeatherApplication/Models/AddressModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WeatherApplication.Models
@@ -19,5 +20,40 @@ namespace WeatherApplication.Models
 
       [Required(ErrorMessage = "State is required.")]
       public string StateCode { get; set; }
+
+      private string addressString, cityString, stateString;
+      private string delimeterPlusSign;
+      private string suffixString;
+
+      public string BuildAddressString(string SelectedStateCode)
+      {
+         SetConfigStrings();
+         StringBuilder address = new StringBuilder();
+         address.Append(addressString); address.Append(StructureNumber);
+         address.Append(delimeterPlusSign); address.Append(StreetName);
+         address.Append(cityString); address.Append(City);
+         address.Append(stateString); address.Append(SelectedStateCode);
+         address.Append(suffixString);
+         address.Replace(' ', '+');
+
+         return address.ToString();
+      }
+   
+      private void SetConfigStrings()
+      {
+         //delimeterPlusSign = _config.GetValue<string>("delimeterPlusSign");
+         delimeterPlusSign = "+";
+         //addressString = _config.GetValue<string>("addressString");
+         addressString = "address?street=";
+         //cityString = _config.GetValue<string>("cityString");
+         cityString = "&city=";
+         //stateString = _config.GetValue<string>("stateString");
+         stateString = "&state=";
+         //suffixString = _config.GetValue<string>("suffixString");
+         suffixString = "&benchmark=9&format=json";
+         //pointsString = _config.GetValue<string>("pointsString");
+      }
+
+
    }
 }
